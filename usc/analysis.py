bcbc1294a024d3f30d4288b6704f108f435b6757
Kahan Dare <kmdare@users.noreply.github.com>
2021-11-04 15:30:06 +0100

    Update analysis.py

--- usc/analysis.py
+++ usc/analysis.py
@@ -5,6 +5,10 @@
 import os
 import re
 import numpy
+import signal
+import scipy.stats as stats
+from typing import Tuple, Callable
+from nptyping import NDArray, Float64
 
 __author__ = "Kahan Dare"
 __credits__ = ["Kahan Dare"]
@@ -14,51 +18,42 @@ __email__ = "kahan.mcaffer.dare@univie.ac.at"
 __status__ = "Development"
 
 
-def load_data(dname, fname, count=-1, offset=0):
+def load_pico(fname: str, count=-1, offset=0) -> Tuple[dict, NDArray[float]]:
     '''Loads picoscope data
 
     args:
-        - dname (string): The relative path from this location to the data
-        file.
-        - fname (string): The file name of the data set.
+        - fname: The relative path from this location to the data file.
 
     returns:
-        - settings (dict): A dictionary containing the settings and comments
-        from the header file.
-        - data (numpy.array): The data in a 2D array with columns being the
-        different channels
+        - settings: A dictionary containing the settings and comments from the
+        header file.
+        - data: The data in a 2D array with columns for each channel
     '''
     # Reading the header file
     header_file_name = fname.replace(".bin", "_header.dat")
-    try:
-        settings = load_header(dname, fname=header_file_name)
-    except FileNotFoundError:
-        print("Could not find {}.".format(header_file_name))
-        print("Attempting to load default header.")
-        settings = load_header(dname)
+    settings = load_header(header_file_name)
 
     # Reading the data file
-    data = numpy.fromfile(os.path.join(dname, fname), dtype=numpy.int16,
-                          count=count, offset=offset)
+    data = numpy.fromfile(fname, dtype=numpy.int16, count=count, offset=offset)
     N_channels = len(settings["Channels"].keys())
     data = data.reshape((len(data)//N_channels, N_channels))
     return (settings, data)
 
 
-def load_header(dname, fname="Header.dat"):
+def load_header(fname: str) -> dict:
     '''Loads the header file containing all the headings
 
     args:
-        - dname (string): The data set directory.
-        - fname (string): The header file's name.
+        - dname: The data set directory.
+        - fname: The header file's name.
 
     returns:
-        - settings (dict): A dictionary containing the settings and comments
-        from the header file.
+        settings: A dictionary containing the settings and comments from the
+        header file.
     '''
     settings = dict()
     channel_dict = dict()
-    with open(os.path.join(dname, fname), "r") as f:
+    with open(fname, "r") as f:
         for i, line in enumerate(f):
             try:
                 label, value = re.split('=|\:', line)
@@ -76,20 +71,38 @@ def load_header(dname, fname="Header.dat"):
     settings["SampleInterval"] = float(settings["SampleInterval"])
     return settings
 
-def estimate_power_spectral_density(x,timestep, bandwidth = 1000):
+
+def chisquare(f: Callable, x: NDArray[float], y: NDArray[float],
+              p: dict[str, float]) -> float:
+    '''Computes the Chi-Square goodness of fit test
+
+    args:
+        - f: The fit function
+        - x: The independent variable
+        - y: The dependent variable
+        - p: The other function parameters
+    '''
+    chi2, _ = stats.chisquare(f_obs=y, f_exp=f(x, p))
+    return chi2
+
+
+def PSD(x: NDArray[float], timestep: float,
+        bandwidth=1000) -> Tuple[NDArray[float], NDArray[float]]:
     '''Takes a given timetrace and computs the power spectral desity
 
     args:
         - x: The given timetrace
         - timestep: The timesteps between the points
+
+    kwargs:
         - bandwidth: The bandwidth of the fft
 
 
     returns:
-        - f (NDArray[Float64]): Array of the sample frequencies
-        - Pxx (NDarray[Float64]): Array of the Power spectral density (only positive side)
+        - f: Array of the sample frequencies
+        - Pxx: Array of the Power spectral density (only positive side)
     '''
-    sample = 1/(timestep * bandwidth)                           #The sample size
-    f, Pxx = signal.welch(x, 1 / timestep, nperseg = sample)    #uses the Signal function to calculate
-    return (f, Pxx) 
+    sample = 1/(timestep * bandwidth)  #The sample size
+    f, Pxx = signal.welch(x, 1 / timestep, nperseg=sample)  # uses the Signal function to calculate
+    return (f, Pxx)
 
